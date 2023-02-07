7434d28d1671b003a24068be22b1e7e79890a095
Anders Jonsson <aj224rj@student.lnu.se>
2023-02-07 10:32:53 +0100

    Add texture to ground

--- src/components/Ground/Ground.tsx
+++ src/components/Ground/Ground.tsx
@@ -1,46 +1,29 @@
-import { MeshReflectorMaterial } from '@react-three/drei'
-import { useLoader } from '@react-three/fiber'
-import React, { useEffect } from 'react'
-import { LinearEncoding, RepeatWrapping, TextureLoader } from 'three'
+import { useTexture } from '@react-three/drei'
+import React from 'react'
 
 const Ground = () => {
-  const [rough, normal] = useLoader(TextureLoader, [
-    `${process.env.PUBLIC_URL}textures/grass-rough.jpg`,
-    `${process.env.PUBLIC_URL}textures/grass-normal.jpg`
+  const [normal, rough] = useTexture([
+    `${process.env.PUBLIC_URL}textures/cobblestone_floor_normal.jpg`,
+    `${process.env.PUBLIC_URL}textures/cobblestone_floor_rough.png`
   ])
 
-  useEffect(() => {
+  /* useEffect(() => {
     [rough, normal].forEach((texture) => {
       texture.wrapS = RepeatWrapping
       texture.wrapT = RepeatWrapping
       texture.repeat.set(5, 5)
       texture.encoding = LinearEncoding
     })
-  }, [rough, normal])
+  }, [rough, normal]) */
 
   return (
     <>
-      <mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
+      <mesh rotation-x={-Math.PI * 0.5} receiveShadow>
         <planeGeometry args={[30, 30]} />
-        <MeshReflectorMaterial
-          mirror={0}
-          envMapIntensity={0}
+        <meshLambertMaterial
+          map={normal}
           normalMap={normal}
-          // normalScale={[0.2, 0.2]}
-          roughnessMap={rough}
-          dithering
-          color={[0.1, 0.1, 0.1]}
-          roughness={0.7}
-          blur={[1000, 400]}
-          mixBlur={30}
-          mixStrength={80}
-          mixContrast={1}
-          resolution={1024}
-          depthScale={0.01}
-          minDepthThreshold={0.9}
-          maxDepthThreshold={1}
-          depthToBlurRatioBias={0.25}
-          reflectorOffset={0.2}
+          alphaMap={rough}
         />
       </mesh>
     </>
--- src/components/Showroom/Showroom.tsx
+++ src/components/Showroom/Showroom.tsx
@@ -6,25 +6,16 @@ const Showroom = () => {
   return (
     <>
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
-      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
-      <color args={[255, 255, 255]} attach='background' />
+      <PerspectiveCamera makeDefault fov={50} position={[10, 10, 25]} />
+      <color args={[0, 0, 0]} attach='background' />
 
+      <pointLight power={15} castShadow position={[10, 3, 37]} />
       <spotLight
-        color={[1, 0.3, 0.8]}
-        intensity={1.5}
+        color={[0.5, 0.3, 0.6]}
+        intensity={0.5}
         angle={0.6}
         penumbra={0.5}
-        position={[5, 5, 0]}
-        castShadow
-        shadow-bias={-0.0002}
-      />
-
-      <spotLight
-        color={[0.25, 0.5, 0.8]}
-        intensity={1.8}
-        angle={0.6}
-        penumbra={0.5}
-        position={[-5, 5, 0]}
+        position={[35, 15, 0]}
         castShadow
         shadow-bias={-0.0002}
       />
